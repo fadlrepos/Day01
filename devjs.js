eb99c4cf0332cc04c915ef7fe7425d68277c45ab
fadlrepos <fadlicloud01@gmail.com>
2026-09-21 11:00:08 +0100

    Add files via upload

---
+++ devjs.js
@@ -0,0 +1,7 @@
+console.log("===== SkyLine Airlines - Check-In =====");
+console.log("Passenger: Karim Benali | Flight: SL204");
+console.log("Seat: 14");
+console.log("Seat: 14A");
+console.log("Priority boarding:", 60>45);
+console.log(typeof 349.99);
+console.log("Gate:",+12, "status: Boarding...");
